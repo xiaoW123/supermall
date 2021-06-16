6c77523f48a42226f5260821590c394444f3b2de
xiaoW123 <wl1223171@outlook.com>
2021-06-16 23:00:49 +0800

    第三次

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,8 +1,26 @@
 <template>
   <div id="home">
     <nav-bar class="nav-bar"><div slot="center">购物车</div></nav-bar>
-    <child-comps :banners="banners"></child-comps> <!-- 轮播图 -->
-    <recommen-view :recommends="recommends"></recommen-view> <!-- 活动福利模块 -->
+    <child-comps :banners="banners"></child-comps> <!-- 轮播图组件 -->
+    <recommen-view :recommends="recommends"></recommen-view> <!-- 活动福利组件 -->
+    <feature-view></feature-view> <!-- 排行榜组件  -->
+    <h2>22222</h2>
+    <h2>22222</h2>
+    <h2>22222</h2>
+    <h2>22222</h2>
+    <h2>22222</h2>
+    <h2>22222</h2>
+    <h2>22222</h2>
+    <h2>22222</h2>
+    <h2>22222</h2>
+    <h2>22222</h2>
+    <h2>22222</h2>
+    <h2>22222</h2>
+    <h2>22222</h2>
+    <h2>22222</h2>
+    <h2>22222</h2>
+    <h2>22222</h2>
+
   </div>
 </template>
 
@@ -12,7 +30,7 @@ import NavBar from 'components/common/navbar/NavBar.vue'
 import {getHomeMutidata} from 'network/home.js'
 import ChildComps from './childComps/ChildComps.vue'
 import RecommenView from './childComps/RecommenView.vue'
-
+import FeatureView from './childComps/FeatureView.vue'
 
 
   export default {
@@ -21,6 +39,7 @@ import RecommenView from './childComps/RecommenView.vue'
       NavBar,
       ChildComps,
       RecommenView,
+      FeatureView,
     },
     data() {
       return {
@@ -41,7 +60,17 @@ import RecommenView from './childComps/RecommenView.vue'
 </script>
 
 <style scoped>
+  #home {
+    padding-top: 44px;
+  }
+
   .nav-bar {
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    z-index: 5;
+
     background-color: var(--color-tint);
     text-align: center;
     color: var(--color-background);
---
+++ src/views/home/childComps/FeatureView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="fature" >
+    <a href="https://act.mogujie.com/zzlx67">
+      <img src="~assets/img/home/recommend_bg.jpg" alt="">
+    </a>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FeatureView'
+}
+</script>
+
+<style>
+  .fature {
+    width: 100%;
+  }
+  .fature img {
+    width: 100%;
+  }
+</style>
